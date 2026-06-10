086e4f75218b5712a087ff493a88b4f9c212259e
k-gappo <kaho-kaho0415@docomo.ne.jp>
2026-06-11 01:51:31 +0900

    移植/画面遷移を実装

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//追加したルーター
+var siriaiRouter = require('./routes/siriai')
+
 var app = express();
 
 // view engine setup
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+//追加した
+app.use('/siriai', siriaiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/siriai.js
@@ -0,0 +1,77 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET 知り合い登録画面を表示 */
+router.get('/new', function (req, res, next) {
+  res.render('siriai/new', { title: '知り合い一覧ページ' });
+});
+
+/* GET 知り合い一覧を表示 */
+router.get('/itiran', function (req, res, next) {
+  res.render('siriai/itiran', { title: '知り合い一覧ページ' });
+});
+
+/* GET 知り合い一覧を表示 */
+router.get('/new/mii', function (req, res, next) {
+  res.render('siriai/mii', { title: 'Mii作成ページ' });
+});
+
+
+
+
+
+
+
+// /* GET 知り合い一覧をDBから取得して表示 */
+// router.get('/', function (req, res, next) {
+//   db.all('SELECT * FROM people', [], function (err, rows) {
+//     res.render('people/index', { title: '知り合い表示ページ', people: rows });
+//   });
+// });
+
+// /* POST 知り合いをDBに登録して一覧にリダイレクト */
+// router.post('/', function (req, res, next) {
+//   var 名前 = req.body['名前'];
+//   var 趣味 = req.body['趣味'];
+//   var 性格 = req.body['性格'];
+//   db.run('INSERT INTO people (名前, 趣味, 性格) VALUES (?, ?, ?)',
+//     [名前, 趣味, 性格],
+//     function (err) {
+//       console.log(err);
+//       res.redirect('/people');
+//     }
+//   );
+// });
+
+// /* POST 指定したidの知り合いをDBから削除して一覧にリダイレクト */
+// router.post('/:id/delete', function (req, res, next) {
+//   var id = req.params.id;
+//   db.run('DELETE FROM people WHERE id = ?', [id], function (err) {
+//     res.redirect('/people');
+//   });
+// });
+
+// /* GET 指定したidの知り合いの編集画面を表示 */
+// router.get('/:id/edit', function (req, res, next) {
+//   var id = req.params.id;
+//   db.get('SELECT * FROM people WHERE id = ?', [id], function (err, row) {
+//     res.render('people/edit', { person: row });
+//   });
+// });
+
+// /* POST 指定したidの知り合いの情報をDBで更新して一覧にリダイレクト */
+// router.post('/:id/update', function (req, res, next) {
+//   var id = req.params.id;
+//   var 名前 = req.body['名前'];
+//   var 趣味 = req.body['趣味'];
+//   var 性格 = req.body['性格'];
+//   db.run('UPDATE people SET 名前=?, 趣味=?, 性格=? WHERE id=?',
+//     [名前, 趣味, 性格, id],
+//     function (err) {
+//       console.log(err);
+//       res.redirect('/people');
+//     }
+//   );
+// });
+
+module.exports = router;
